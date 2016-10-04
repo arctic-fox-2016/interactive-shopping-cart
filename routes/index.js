9aa3348bb03720db9e65d6298cbdd8edb8dc7a2f
Andrew <atandiawan@gmail.com>
2016-10-04 18:54:40 +0700

    Before Jquery

--- routes/index.js
+++ routes/index.js
@@ -16,25 +16,50 @@ router.get('/display', function(req, res, next) {
   http.get({
     host: "localhost",
     port: "3000",
-    path: "/api/display"
+    path: "/api/display/customer"
   }, function(response) {
     let hasil = ""
     response.on('data', function(d) {
       hasil += d
     });
     response.on('end', function() {
-      let hasilJSON = JSON.parse(hasil)
-      res.render('display.ejs', { customers: hasilJSON })
+      let hasilJSONCustomers = JSON.parse(hasil)
+      http.get({
+        host: "localhost",
+        port: "3000",
+        path: "/api/display/item"
+      }, function(response) {
+        hasil = ""
+        response.on('data', function(d) {
+          hasil += d
+        });
+        response.on('end', function() {
+          let hasilJSONItems = JSON.parse(hasil)
+          http.get({
+            host: "localhost",
+            port: "3000",
+            path: "/api/display/cart"
+          }, function(response) {
+            hasil = ""
+            response.on('data', function(d) {
+              hasil += d
+            });
+            response.on('end', function() {
+              let hasilJSONCarts = JSON.parse(hasil)
+              res.render('display.ejs', { customers: hasilJSONCustomers, items: hasilJSONItems, carts:hasilJSONCarts })
+            });
+          })
+        });
+      })
     });
   })
 })
 
-//
-router.post('/edit/:id', function(req, res, next) {
+router.post('/customers/edit/:id', function(req, res, next) {
   http.get({
     host: "localhost",
     port: "3000",
-    path: `/api/edit/${req.params.id}`,
+    path: `/api/customers/edit/${req.params.id}`,
   }, function(response) {
     let hasil = ""
     response.on('data', function(d) {
@@ -42,7 +67,24 @@ router.post('/edit/:id', function(req, res, next) {
     });
     response.on('end', function() {
       let hasilJSON = JSON.parse(hasil)
-      res.render('edit.ejs', { customer: hasilJSON })
+      res.render('edit-customer.ejs', { customer: hasilJSON })
+    });
+  })
+})
+
+router.post('/items/edit/:id', function(req, res, next) {
+  http.get({
+    host: "localhost",
+    port: "3000",
+    path: `/api/items/edit/${req.params.id}`,
+  }, function(response) {
+    let hasil = ""
+    response.on('data', function(d) {
+      hasil += d
+    });
+    response.on('end', function() {
+      let hasilJSON = JSON.parse(hasil)
+      res.render('edit-item.ejs', { item: hasilJSON })
     });
   })
 })
@@ -74,10 +116,6 @@ router.post('/api/register/cart', function(req, res, next){
       } else {
         for (let j in req.body.item_code){
           Models.Items.findOne({item_code: req.body.item_code[j]}, function(err, result3){
-            console.log(req.body.item_code[j])
-            console.log('nemu item')
-            console.log(result2)
-            console.log(result3)
             Models.Carts.update({ _id: result2._id}, { $push: {itemList:{"item_detail": result3._id, "qty": req.body.qty[j]}}}, function(err,result4){
               if(err){
                 console.log(err)
@@ -93,7 +131,7 @@ router.post('/api/register/cart', function(req, res, next){
 })
 
 
-router.get('/api/display/user', function(req, res, next) {
+router.get('/api/display/customer', function(req, res, next) {
   Models.Customers.find({}, function(err, result) {
     if (err) {
       console.log(err)
@@ -123,7 +161,7 @@ router.get('/api/display/cart', function(req, res, next) {
   })
 })
 
-router.post('/api/delete/:id', function(req, res, next) {
+router.post('/api/customers/delete/:id', function(req, res, next) {
   Models.Customers.remove({ _id: req.params.id }, function(err) {
     if (err) {
       console.log(err)
@@ -133,16 +171,39 @@ router.post('/api/delete/:id', function(req, res, next) {
   })
 })
 
-router.get('/api/edit/:id', function(req, res, next) {
+router.get('/api/customers/edit/:id', function(req, res, next) {
   Models.Customers.findOne({ _id: req.params.id }, function(err, result) {
     res.json(result)
   })
 })
 
-router.post('/api/edit/success/:id', function(req, res, next) {
+router.post('/api/customers/edit/success/:id', function(req, res, next) {
   Models.Customers.update({ _id: req.params.id }, {member_id: req.body.member_id, name: req.body.name, address: req.body.address, zipcode: req.body.zipcode, phone: req.body.phone}, function(err){
     res.redirect("/display")
   })
 })
+
+router.post('/api/items/delete/:id', function(req, res, next) {
+  Models.Items.remove({ _id: req.params.id }, function(err) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.redirect("/display")
+    }
+  })
+})
+
+router.get('/api/items/edit/:id', function(req, res, next) {
+  Models.Items.findOne({ _id: req.params.id }, function(err, result) {
+    res.json(result)
+  })
+})
+
+router.post('/api/items/edit/success/:id', function(req, res, next) {
+  Models.Items.update({ _id: req.params.id }, {item_code: req.body.item_code, name: req.body.name, description: req.body.description, price: req.body.price, stock: req.body.stock}, function(err){
+    res.redirect("/display")
+  })
+})
+
 
 module.exports = router
